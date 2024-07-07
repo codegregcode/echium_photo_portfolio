b9edc1119503deef4a6e9cecc203ded1e4d29384
Greg Cain <codegregcode@gmail.com>
2024-07-07 16:52:24 +0100

    imports Footer into App and updates App snapshot

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 
 import Welcome from './Welcome';
 import Index from './Index';
+import Footer from './Footer';
 
 import Mar172024 from './journals/twenty24/Mar172024';
 import Apr272024 from './journals/twenty24/Apr272024';
@@ -11,12 +12,15 @@ import '../styles/App.css';
 function App() {
   return (
     <div className="app-container">
-      <Routes>
-        <Route path="/" element={<Welcome />} />
-        <Route path="/index" element={<Index />} />
-        <Route path="/index/a-test-a-start" element={<Mar172024 />} />
-        <Route path="/index/porto" element={<Apr272024 />} />
-      </Routes>
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/index" element={<Index />} />
+          <Route path="/index/a-test-a-start" element={<Mar172024 />} />
+          <Route path="/index/porto" element={<Apr272024 />} />
+        </Routes>
+      </div>
+      <Footer />
     </div>
   );
 }
