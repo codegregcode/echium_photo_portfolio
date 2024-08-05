cb98a76741bdf7ab441978e4fa58d7e79ad2708d
Greg Cain <codegregcode@gmail.com>
2024-08-05 13:43:00 +0100

    imports new componets into App and Route setup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,6 +6,8 @@ import Footer from './Footer';
 
 import Mar172024 from './journals/twenty24/Mar172024';
 import Apr272024 from './journals/twenty24/Apr272024';
+import May2024 from './journals/twenty24/May2024';
+import Jun2024 from './journals/twenty24/Jun2024';
 
 import '../styles/App.css';
 
@@ -18,6 +20,8 @@ function App() {
           <Route path="/index" element={<Index />} />
           <Route path="/index/a-test-a-start" element={<Mar172024 />} />
           <Route path="/index/porto" element={<Apr272024 />} />
+          <Route path="/index/testing-the-a1" element={<May2024 />} />
+          <Route path="/index/starting-to-develop" element={<Jun2024 />} />
         </Routes>
       </div>
       <Footer />
